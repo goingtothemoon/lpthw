c0f44c0881550d893722d030a8afa6b94608b05b
Richard Fernandez <kcomico@gmail.com>
2015-12-20 15:57:47 -0500

    updates

--- Week 3 ex34-40/ex34.py
+++ Week 3 ex34-40/ex34.py
@@ -1,76 +1,22 @@
-from sys import exit
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+print animals
+for i in animals:
+	print i
 
-def gold_room():
-	print "This room is full of gold. How much do you take?"
+print ""
+print "First, Second, third are ordinal numbers but we"
+print "need to count by cardinal numbers : 1, 2, 3,"
+print "With lists, you start at 0."
+print ""
 
-	choice = raw_input("-> ")
-	if "0" in choice or "1" in choice:
-		how_much = int(choice)
-	else:
-		dead("Man, learn to type a number.")
+# To access a particular item in a list, put the list
+# name and then index number in square brackets
 
-	if how_much < 50:
-		print "Nice, you're not greedy, you win!"
-		exit(0)
-	else:
-		dead("You greedy bastard!")
+for i in range(len(animals)):
+	print "Index %d in the list is %s" % (i, animals[i])
 
+# for i which is variable animals. 
+# len means length (however long it is do it that many times)
 
-def bear_room():
-	print "There is a bear here."
-	print "The bear has a bunch of honey."
-	print "The fat bear is in front of another door."
-	print "How are you going to move the bear?"
-	bear_moved = False
-
-	while True:
-		choice = raw_input("-> ")
-
-		if choice == "take honey":
-			dead("The bear looks at you then slaps your face off.")
-		elif choice == "taunt bear" and not bear_moved:
-			print "The beat has moved from the door. You can go through it now."
-			bear_moved = True
-		elif choice == "taunt bear" and bear_moved:
-			dead("The beat gets pissed off and chews your leg off.")
-		elif choice == "open door" and bear_moved:
-			gold_room()
-		else:
-			print "I got no idea what that means."
-
-
-def chtulhu_room():
-	print "Here you see the great evil Chutulu."
-	print "He, it, whatever stared at you and you go insane."
-	print "Do you flee for your life or eat your head?"
-
-	choice = raw_input("-> ")
-
-	if "flee" in choice:
-		start()
-	elif "head" in choice:
-		dead("Well that was tasty!")
-	else:
-		chtulhu_room()
-
-
-def dead(why):
-	print why, "Good job!"
-	exit(0)
-
-def start():
-	print "You are in a dark room."
-	print "There is a door to your right and left."
-	print "Which one do you take?"
-
-	choice = raw_input("-> ")
-
-	if choice =="left":
-		bear_room()
-	elif choice == "right":
-		chtulhu_room()
-	else:
-		dead("You stumble around the room until you starve.")
-
-
-		start()
+print "\n"
+print animals[4]
